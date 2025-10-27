f62cef75ae69ace639dbe8a4f3cdb9e67c1d1006
Suki <tues1205@naver.com>
2025-10-27 19:23:57 +0900

    💄 Design: IconButton 클릭 효과 추가

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
+import s from './style/IconButton.module.scss';
 
 const IconButton = ({ src, alt, width, onClick }) => {
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      style={{
-        display: 'inline-flex',
-        background: 'none',
-        border: 'none',
-        padding: 0,
-        margin: 0,
-        cursor: 'pointer',
-      }}
-    >
+    <button type="button" onClick={onClick} className={s.iconButtonContainer}>
       <img src={src} alt={alt} width={width} />
     </button>
   );
