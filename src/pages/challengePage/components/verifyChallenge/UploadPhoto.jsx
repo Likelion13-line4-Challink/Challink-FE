2ce40931226181b000b3b8e2ea7f2f405810eb01
Suki <tues1205@naver.com>
2025-10-29 04:19:03 +0900

    ✨ Feat: 사진 업로드 기능

--- src/pages/challengePage/components/verifyChallenge/UploadPhoto.jsx
+++ src/pages/challengePage/components/verifyChallenge/UploadPhoto.jsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import s from '../style/UploadPhoto.module.scss';
 import GradientButton from '../../../../components/GradientButton';
 import CHAR from '@assets/images/character.png';
 
 const UploadPhoto = () => {
+  const [photo, setPhoto] = useState(null); // 업로드 사진
+  const fileInputRef = useRef(null);
+
+  // 파일 선택 input 여는 함수
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  // 사진 업로드
+  const handlePhotoUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setPhoto(URL.createObjectURL(file));
+    }
+  };
+
   return (
     <div className={s.uploadPhotoContainer}>
       <div className={s.photo}>
-        {/* 사진 없는 경우 */}
-        <div className={s.noUpload}>
-          <div className={s.bubble}>
-            <p>혹시… 오늘 인증 깜빡하신 건 아니죠?</p>
+        {photo ? (
+          <img src={photo} alt="업로드된 사진" className={s.uploadedPhoto} />
+        ) : (
+          // 사진 안올린 경우
+          <div className={s.noUpload}>
+            <div className={s.bubble}>
+              <p>혹시… 오늘 인증 깜빡하신 건 아니죠?</p>
+            </div>
+            <img src={CHAR} alt="캐릭터" width="83px" />
           </div>
-          <img src={CHAR} alt="캐릭터" width="83px" />
-        </div>
+        )}
       </div>
 
       {/* 사진올리기 버튼 */}
-      <GradientButton width="255px" height="48px" text="사진올리기" />
+      <GradientButton width="255px" height="48px" text="사진올리기" onClick={handleButtonClick} />
+
+      {/* 숨겨진 파일 첨부 input */}
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={handlePhotoUpload}
+      />
     </div>
   );
 };
