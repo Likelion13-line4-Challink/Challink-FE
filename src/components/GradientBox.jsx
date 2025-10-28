17607f3194d88f8a57e6db62c24a0aee05eb8c5a
Suki <tues1205@naver.com>
2025-10-29 03:42:21 +0900

    💄 Design: UploadPhoto UI 구현

--- src/components/GradientBox.jsx
+++ src/components/GradientBox.jsx
@@ -4,7 +4,7 @@ import s from './style/GradientBox.module.scss';
 const GradientBox = ({ width, height, text, borderRadius }) => {
   return (
     <div className={s.gradientBox} style={{ width, height, borderRadius }}>
-      <p>{text}</p>
+      <div className={s.gradientText}>{text}</div>
     </div>
   );
 };
--- src/pages/challengePage/components/verifyChallenge/UploadPhoto.jsx
+++ src/pages/challengePage/components/verifyChallenge/UploadPhoto.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import s from '../style/UploadPhoto.module.scss';
 import GradientButton from '../../../../components/GradientButton';
+import CHAR from '@assets/images/character.png';
 
 const UploadPhoto = () => {
   return (
     <div className={s.uploadPhotoContainer}>
-      <div className={s.photo}>사진</div>
+      <div className={s.photo}>
+        {/* 사진 없는 경우 */}
+        <div className={s.noUpload}>
+          <div className={s.bubble}>
+            <p>혹시… 오늘 인증 깜빡하신 건 아니죠?</p>
+          </div>
+          <img src={CHAR} alt="캐릭터" width="83px" />
+        </div>
+      </div>
+
+      {/* 사진올리기 버튼 */}
       <GradientButton width="255px" height="48px" text="사진올리기" />
     </div>
   );
