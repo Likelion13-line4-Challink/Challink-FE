c7a85478326e1b9f9226013a8f9999d4943a7e36
Suki <tues1205@naver.com>
2025-10-31 04:14:54 +0900

    💄 Design: 모든사진페이지 UI 구현(필터링 제외)

--- src/pages/photosPage/PhotosPage.jsx
+++ src/pages/photosPage/PhotosPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ChallengeBody from '../challengeLayout/ChallengeBody';
 import PhotosChallenge from './components/PhotosChallenge';
+import data from './datas/photosDummy.json';
 
 const PhotosPage = () => {
   return (
     <>
-      <div>필터링</div>
+      <div style={{ width: '100%', height: '32px', backgroundColor: 'red' }}>필터링</div>
       <ChallengeBody height="624px">
-        <PhotosChallenge />
+        <PhotosChallenge photoData={data} />
       </ChallengeBody>
     </>
   );
