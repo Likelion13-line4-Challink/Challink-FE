23fd63d6df57c2770d8f63f62e8c06c1a111883c
Suki <tues1205@naver.com>
2025-11-04 20:55:58 +0900

    💄 Design: PointItem 컴포넌트

---
+++ src/pages/myPage/components/PointItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './styles/PointItem.module.scss';
+
+const PointItem = () => {
+  return (
+    <div className={s.pointItemContainer}>
+      <p className={s.type}>참가</p>
+      <div>
+        <p className={s.title}>⏰ 매일 7시 기상팟 모집</p>
+        <p className={s.date}>2025.10.26</p>
+      </div>
+      <div className={s.point}>- 30,000 p</div>
+    </div>
+  );
+};
+
+export default PointItem;
