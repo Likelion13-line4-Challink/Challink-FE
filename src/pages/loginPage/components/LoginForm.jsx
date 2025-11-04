55544989d34738dad66ef115d05b5a8cd8ce5424
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-04 21:26:15 +0900

    💄 Design : 로그인 컴포넌트 UI

---
+++ src/pages/loginPage/components/LoginForm.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import s from './styles/LoginForm.module.scss';
+
+import {
+  EmailIcon,
+  PasswordIcon,
+  EyeIcon,
+  EyeCloseIcon,
+  CheckIcon,
+  CheckFillIcon,
+} from '../components/icon/index';
+
+const LoginForm = () => {
+  const [visible, setVisible] = useState(true);
+  const [idSave, setIdSave] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div className={s.LoginFormContainer}>
+      <div className={s.inputField}>
+        <label className={s.inputLabel} htmlFor="email">
+          이메일
+        </label>
+        <div className={s.emailWrapper}>
+          <EmailIcon style={{ color: email.trim() ? '#111' : '#777C89' }} />
+
+          <input
+            className={s.inputBox}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="이메일을 입력해 주세요"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className={s.inputField}>
+        <label className={s.inputLabel} htmlFor="password">
+          비밀번호
+        </label>
+        <div className={s.passwordWrapper}>
+          <PasswordIcon style={{ color: password.trim() ? '#111' : '#777C89' }} />
+          <input
+            className={s.inputBox}
+            type={visible ? 'password' : 'text'}
+            id="password"
+            name="password"
+            placeholder="비밀번호를 입력해 주세요"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="button"
+            className={s.togglePasswordBtn}
+            onClick={() => setVisible(!visible)}
+            aria-label={visible ? '비밀번호 숨기기' : '비밀번호 보기'}
+          >
+            {visible ? <EyeCloseIcon /> : <EyeIcon />}
+          </button>
+        </div>
+      </div>
+
+      <div className={s.idSaveContainer}>
+        <button type="button" onClick={() => setIdSave(!idSave)} className={s.idSaveBtn}>
+          {idSave ? <CheckFillIcon /> : <CheckIcon />} 아이디 저장
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
