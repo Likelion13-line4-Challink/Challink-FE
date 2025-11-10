3cbad23c84f0318a8bdd6033a6b20f3129e53342
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-11 01:17:54 +0900

    ✨ Feat : 챌린지 도전 api 연동

--- src/components/challengeModal/ChallengeModal.jsx
+++ src/components/challengeModal/ChallengeModal.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { challengeDetailApi } from '@apis/auth/challengeApi';
-
+import { challengeDetailApi, joinChallengeApi } from '@apis/auth/challengeApi';
+import useNavigation from '../../hooks/useNavigation';
 import s from './ChallengeModal.module.scss';
+import Popup from '../Popup';
 import closeIcon from '../../assets/images/icon_close.svg';
 import checkFillIcon from '../../assets/images/check_fill_icon.svg';
 import checkIcon from '../../assets/images/check_icon.svg';
 import GradientButton from '../../components/GradientButton';
 
 const ChallengeModal = ({ onClose, challengeId }) => {
+  const { goTo } = useNavigation();
+
   // API 데이터 관리
   const [challenge, setChallenge] = useState(null);
   // const [loading, setLoading] = useState(false);
@@ -17,7 +20,6 @@ const ChallengeModal = ({ onClose, challengeId }) => {
   // 모달 상태 관리
   useEffect(() => {
     // if (!challengeId) return; // 필요한 지 모르겠음
-
     const fetchDetail = async () => {
       try {
         // setLoading(true);
@@ -28,16 +30,62 @@ const ChallengeModal = ({ onClose, challengeId }) => {
       } // finally {
       //   setLoading(false); }
     };
-
     fetchDetail();
   }, [challengeId]); // challengeId가 바뀔 때마다 실행
 
+  // 동의하기 버튼
   const handleAgreeClick = () => {
     setAgreed((prev) => !prev);
   };
 
-  const handleSubmit = () => {
+  // 에러/로딩 상태
+  const [apiError, setApiError] = useState('');
+  const [errorTitle, setErrorTitle] = useState('');
+
+  // 팝업 확인 버튼
+  const [showPopup, setShowPopup] = useState(false);
+  const handlePopupConfirm = () => {
+    setShowPopup(false);
+    goTo('/');
+  };
+
+  // 도전하기 버튼
+  const handleSubmit = async (e) => {
     if (!agreed) return;
+    e.preventDefault();
+    try {
+      // 성공
+      const result = await joinChallengeApi(challengeId, { agree_terms: agreed });
+      console.log(result);
+    } catch (err) {
+      if (err.response) {
+        const { status, data } = err.response;
+        setShowPopup(true); // 실패 시 팝업 열기
+
+        if (status === 400) {
+          setApiError(data.message || '이미 참가 중인 챌린지입니다.');
+          setErrorTitle('참가중인 챌린지');
+        } else if (status === 401) {
+          setApiError(data.message || '인증이 누락되었습니다.');
+          setErrorTitle('인증 누락');
+        } else if (status === 403) {
+          setApiError(data.message || '이 챌린지는 현재 참여가 불가합니다.');
+          setErrorTitle('참여 불가능');
+        } else if (status === 404) {
+          setApiError(data.message || '챌린지가 없습니다.');
+          setErrorTitle('챌린지 없음');
+        } else if (status === 409) {
+          setApiError(data.message || '포인트가 부족하거나 정원이 모두 찼습니다.');
+          setErrorTitle('포인트 부족');
+        } else {
+          setApiError('서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.');
+        }
+      } else {
+        setApiError('인터넷 연결을 확인해주세요.');
+      }
+      // } finally {
+      //   setLoading(false);
+    }
   };
 
   return (
@@ -57,12 +105,12 @@ const ChallengeModal = ({ onClose, challengeId }) => {
         </div>
 
         {/* 커버 */}
-        <img src={challenge.over_image} className={s.coverImage} alt="" />
+        <img src={challenge.cover_image} className={s.coverImage} alt="" />
 
         {/* 상세 카드 */}
         <div className={s.challengeInfoCard}>
           <h3 className={s.challengeInfo}>
-            {challenge.entry_fee.toLochallengealeString()}p 걸고 {challenge.duration_weeks}주 동안{' '}
+            {challenge.entry_fee.toLocaleString()}p 걸고 {challenge.duration_weeks}주 동안{' '}
             {challenge.freq_type} 인증하기!
           </h3>
 
@@ -103,6 +151,16 @@ const ChallengeModal = ({ onClose, challengeId }) => {
           onClick={handleSubmit}
           disabled={!agreed}
         />
+        {/* 팝업 조건부 렌더링 */}
+        {showPopup && (
+          <Popup
+            type="success"
+            title={errorTitle}
+            subtitle={apiError}
+            buttonText="확인"
+            onClick={handlePopupConfirm}
+          />
+        )}
       </section>
     </div>
   );
