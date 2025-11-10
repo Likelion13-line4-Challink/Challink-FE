7d05b630f08155d870832fb05b84bb4681b47fd7
Suki <tues1205@naver.com>
2025-11-11 03:50:12 +0900

    💄 Design: AiLoadingBox overlay 추가

--- src/pages/verifyPage/components/AiLoadingBox.jsx
+++ src/pages/verifyPage/components/AiLoadingBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from '../style/AiLoadingBox.module.scss';
+import s from './style/AiLoadingBox.module.scss';
 import Bubble from './Bubble';
 import useEllipsis from '../../../hooks/useEllipsis';
 import CHAR from '@assets/images/character.png';
@@ -8,14 +8,16 @@ const AILoadingBox = () => {
   const ellipsis = useEllipsis(500); // ... 애니메이션
 
   return (
-    <div className={s.aiLodingContainer}>
-      <Bubble
-        width="133px"
-        height="52px"
-        text={`AI가 사진을 분석하고\n 있어요${ellipsis}`}
-        fontSize="12px"
-      />
-      <img src={CHAR} alt="캐릭터" width="63.44px" />
+    <div className={s.overlay}>
+      <div className={s.aiLodingContainer}>
+        <Bubble
+          width="133px"
+          height="52px"
+          text={`AI가 사진을 분석하고\n 있어요${ellipsis}`}
+          fontSize="12px"
+        />
+        <img src={CHAR} alt="캐릭터" width="63.44px" />
+      </div>
     </div>
   );
 };
