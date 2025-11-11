c8c2ac4a200056e81255509d763ae412c416630b
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-12 01:41:41 +0900

    🤡 Chore : 주석 삭제

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -7,11 +7,10 @@ import useModalStore from '../../store/modalStore';
 import ChallengeModal from '../../components/challengeModal/ChallengeModal';
 
 const MainPage = () => {
-  // 1. (무한 루프 방지) 스토어 구독을 '따로' 합니다.
+  // 무한 루프 방지
   const modalData = useModalStore((state) => state.modalData);
   const closeModal = useModalStore((state) => state.closeModal);
 
-  // 2. 렌더링 함수
   const renderGlobalModal = () => {
     if (!modalData) return null;
 
@@ -25,17 +24,13 @@ const MainPage = () => {
   };
 
   return (
-    // 3. React.Fragment로 감싸기
     <React.Fragment>
-      {/* 3-1. 페이지 본문 */}
       <div>
         <Header />
         <MyChallenge />
         <AllChallenge />
         <Footer />
       </div>
-
-      {/* 3-2. 모달 렌더링 위치 */}
       {renderGlobalModal()}
     </React.Fragment>
   );
